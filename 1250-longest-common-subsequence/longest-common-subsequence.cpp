350111145eb70ae43cf4f2f2112f288565301a50
Shourya Sinha <shouryasinha23@gmail.com>
2026-03-28 19:22:03 +0530

    Time: 42 ms (26.74%) | Memory: 27.7 MB (15.56%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int solve(int n,int m,string &a,string &b,vector<vector<int>> &dp){
-        if(n<0 || m<0)return 0;
+        if(n==0 || m==0)return 0;
         if(dp[n][m]!=-1)return dp[n][m];
-        if(a[n]==b[m]){
+        if(a[n-1]==b[m-1]){
             return dp[n][m]=1+solve(n-1,m-1,a,b,dp);
         }
         return dp[n][m]=max(solve(n-1,m,a,b,dp), solve(n,m-1,a,b,dp));
@@ -11,7 +11,7 @@ public:
     int longestCommonSubsequence(string a, string b) {
         int n=a.size();
         int m=b.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,a,b,dp);        
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return solve(n,m,a,b,dp);        
 }
 };
