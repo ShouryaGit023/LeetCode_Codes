28c1ea7f09b7ff195568695afd96b011af20ab47
Shourya Sinha <shouryasinha23@gmail.com>
2026-03-29 13:17:11 +0530

    Time: 4 ms (5.16%) | Memory: 9.6 MB (5.81%) - LeetSync

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        multiset<char> o1,o2,e1,e2;
+        int n=s1.size();
+        int m=s2.size();
+        for(int i=0;i<n;i++){
+            if(i & 1){
+                o1.insert(s1[i]);
+            }
+            else{
+                e1.insert(s1[i]);
+            }
+        }
+        for(int i=0;i<m;i++){
+            if( i& 1){
+                o2.insert(s2[i]);
+            }
+            else e2.insert(s2[i]);
+        }
+        
+        return (o1==o2 && e1==e2);
+    }
+};
