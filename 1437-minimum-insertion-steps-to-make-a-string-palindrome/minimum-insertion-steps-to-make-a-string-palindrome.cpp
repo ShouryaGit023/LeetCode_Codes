35ce6ae585c519b981068e24d358547c9a4b06e9
Shourya Sinha <shouryasinha23@gmail.com>
2026-03-29 16:03:31 +0530

    Time: 36 ms (32.66%) | Memory: 31.5 MB (39.01%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -12,9 +12,22 @@ public:
         int n=s.size();
         string a=s;
         reverse(a.begin(),a.end());
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        int lngstPal=solve(n-1,n-1,s,a,dp);
-        cout<<lngstPal;
-        return n-lngstPal;
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                //match 
+                if(s[i-1]==a[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return n-dp[n][n];
     }
 };
