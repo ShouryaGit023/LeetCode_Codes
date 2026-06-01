843e9d5af13b3f7a0e08a6e298dc1722ea49ad6b
Shourya Sinha <shouryasinha23@gmail.com>
2026-06-01 20:39:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (86.67%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dp[101][101];
+    int rob(vector<int>& v) {
+        int n=v.size();
+        memset(dp,0,sizeof(dp));
+        if(n==1)return v[0];
+        dp[0][0]=v[0];
+        dp[1][0]=max(v[0],v[1]);
+
+        dp[1][1]=v[1];
+        for(int i=2;i<n-1;i++){
+            dp[i][0]=max(dp[i-1][0],v[i]+dp[i-2][0]);
+        }
+        for(int i=2;i<n;i++){
+            dp[i][1]=max(dp[i-1][1],dp[i-2][1]+v[i]);
+        }
+        return max(dp[n-2][0],dp[n-1][1]);
+    }
+};
