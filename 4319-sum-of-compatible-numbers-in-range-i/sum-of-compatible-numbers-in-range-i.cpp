e2cba6fba67492780e7365ab19f3df8d98bf564e
Shourya Sinha <shouryasinha23@gmail.com>
2026-06-07 15:44:11 +0530

    Time: 79 ms (3.13%) | Memory: 8.8 MB (3.11%) - LeetSync

---
+++ 4319-sum-of-compatible-numbers-in-range-i/sum-of-compatible-numbers-in-range-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int sumOfGoodIntegers(int n, int k) {
+        int ans=0;
+        for(int i=1;i<=100001;i++){
+            if(abs(n-i)<=k && (n&i)==0)ans+=i;
+        }
+        return ans;
+    }
+};
