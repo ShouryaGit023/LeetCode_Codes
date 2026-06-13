f9964d714da4480eefb6cb70cb9a26392bc96fe7
Shourya Sinha <shouryasinha23@gmail.com>
2026-06-13 19:49:31 +0530

    Time: 1 ms (89.38%) | Memory: 12.4 MB (64.75%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    int solve(int id,vector<int> &v, int t,vector<vector<int>> &dp){
-        if(id<0){
-            return t==0;
+    int findTargetSumWays(vector<int>& v, int target) {
+        int sum=accumulate(v.begin(),v.end(),0);
+        int rs=sum+target;
+        if(abs(target)>sum)return 0;
+        if(rs & 1)return 0;
+        rs/=2;
+        vector<int> dp(rs+1,0);
+        dp[0]=1;
+        for(auto i:v){
+            for(int j=rs;j>=i;j--){
+                dp[j]+=dp[j-i];
+            }
         }
-        if(dp[id][t]!=-1)return dp[id][t];
-        int nt=solve(id-1,v,t,dp);
-        int p=0;
-        if(v[id]<=t){
-            p=solve(id-1,v,t-v[id],dp);
-        }
-        return dp[id][t]=p+nt;
-    }
-    int findTargetSumWays(vector<int>& v, int t) {
-        int n=v.size();
-        int a=0;
-        for(auto i:v)a+=i;
-        if (abs(t) > a) return 0;
-        int sum=t+a;
-        if(sum & 1)return 0;
-        vector<vector<int>> dp(n,vector<int>(sum/2 + 1,-1));
-        return solve(n-1,v,sum/2,dp);
-        
+        return dp[rs];
     }
 };
