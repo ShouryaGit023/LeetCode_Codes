c67b0c2dcc568ae3ac6561eabbb39aaa8e066f9b
Shourya Sinha <shouryasinha23@gmail.com>
2026-06-16 21:26:35 +0530

    Time: 15 ms (27.21%) | Memory: 12.6 MB (77.80%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -2,40 +2,33 @@ class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& v) {
         int n=v.size();
-        int lar=1;
-        int larid=0;
-        vector<int> hash(n,0);
         sort(v.begin(),v.end());
         vector<int> dp(n,1);
+        vector<int> last(n,0);
+        for(int i=0;i<n;i++)last[i]=i;
         for(int i=1;i<n;i++){
-            hash[i]=i;
             for(int j=0;j<i;j++){
-                if(v[i]%v[j]==0 &&dp[i]<dp[j]+1){
+                if(dp[i]<dp[j]+1 && v[i]%v[j]==0){
                     dp[i]=dp[j]+1;
-                    hash[i]=j;
-
-                }
-                if(dp[i]>lar){
-                    lar=dp[i];
-                    larid=i;
+                    last[i]=j;
                 }
             }
-            if(dp[i]>lar){
-                lar=dp[i];
-                larid=i;
+        }
+        int lis=-1;
+        int mid=-1;
+        for(int i=0;i<n;i++){
+            if(dp[i]>lis){
+                lis=dp[i];
+                mid=i;
             }
         }
-        vector<int> result;
-        result.push_back(v[larid]);
-        
-        while (hash[larid] != larid) {
-            larid = hash[larid];
-            result.push_back(v[larid]);
+        vector<int> ans;
+        while(1){
+            ans.push_back(v[mid]);
+            if(mid==last[mid])break;
+            mid=last[mid];
         }
-
-        reverse(result.begin(), result.end());
-
-       return result;
+        return ans;
         
     }
 };
