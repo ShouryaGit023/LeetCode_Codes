1cbc9f29bd11203c689331ec2e58ff40a85e0d6c
Shourya Sinha <shouryasinha23@gmail.com>
2026-08-14 20:00:18 +0530

    Time: 584 ms (81.09%) | Memory: 184.1 MB (57.42%) - LeetSync

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    struct Trie {
+        Trie* bit[2];
+        
+        Trie() { // 1. Added ()
+            bit[0] = nullptr;
+            bit[1] = nullptr;
+        }
+
+        void insert(int n) {
+            Trie* curr = this;
+            for (int i = 31; i >= 0; i--) {
+                int b = (n >> i) & 1;
+                if (curr->bit[b] == nullptr) {
+                    curr->bit[b] = new Trie();
+                }
+                curr = curr->bit[b];
+            }
+        }
+
+        int solve(int n) {
+            int ans = 0;
+            Trie* curr = this;
+            for (int i = 31; i >= 0; i--) {
+                int d = (n >> i) & 1;
+                if (curr->bit[1 - d]) {
+                    ans |= (1 << i);
+                    curr = curr->bit[1 - d];
+                } else {
+                    curr = curr->bit[d];
+                }
+            }
+            return ans;
+        }
+    };
+
+    int findMaximumXOR(vector<int>& v) {
+        Trie t;
+        for (auto i : v) {
+            t.insert(i);
+        }
+        int ans = 0;
+        for (auto i : v) {
+            ans = max(ans, t.solve(i)); 
+        }
+        return ans;
+    }
+};
