b97b21097944a6f3ea22631b01ae4b993dc062d9
Shourya Sinha <shouryasinha23@gmail.com>
2026-09-05 16:38:48 +0530

    Time: 16 ms (65.75%) | Memory: 34.7 MB (76.13%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        
-        queue<pair<int, int>> q;
-        vector<vector<int>> ans(n, vector<int>(m, -1));
-
-        // 1. Push all 0s into the queue and mark them as 0 in ans
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
-                    ans[i][j] = 0;
-                    q.push({i, j});
+    vector<vector<int>> updateMatrix(vector<vector<int>>& v) {
+        int n=v.size();
+        int m=v[0].size();
+        vector<vector<int>> ans(n,vector<int>(m,INT_MAX));
+        queue<pair<int,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(v[i][j]==0){
+                    ans[i][j]=0;
+                    q.push({i,j});
                 }
             }
         }
-
-        int dx[4] = {-1, 1, 0, 0};
-        int dy[4] = {0, 0, -1, 1};
-
-        // 2. Perform Multi-Source BFS
-        while (!q.empty()) {
-            auto [x, y] = q.front();
+        int dx[4]={-1,1,0,0};
+        int dy[4]={0,0,1,-1};
+        while(!q.empty()){
+            auto [x,y]=q.front();
             q.pop();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                // If neighbor is within bounds and unvisited (-1)
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && ans[nx][ny] == -1) {
-                    ans[nx][ny] = ans[x][y] + 1;
-                    q.push({nx, ny});
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                if(nx>=0 && nx<n && ny>=0 && ny<m && v[nx][ny]==1 && ans[x][y]+1<ans[nx][ny]){
+                    ans[nx][ny]=ans[x][y]+1;
+                    q.push({nx,ny});
                 }
             }
-        }
 
+        }
         return ans;
+
     }
 };
